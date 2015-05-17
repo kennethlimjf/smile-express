caa6f6cb4f2fede82c8e9d59551e25fc1f3f6495
Kenneth Lim <kennethlimjf@gmail.com>
2015-05-17 23:55:58 +0800

    Set route for driver edit. Create new view for driver edit.

--- routes/admin/drivers.js
+++ routes/admin/drivers.js
@@ -7,15 +7,15 @@ const
 router.param('userId', function(request, response, next, userId) {
   new DriverQuery()
     .find(userId)
-    .then(function(user) {
-      request.user = user;
+    .then(function(driver) {
+      request.driver = driver;
       return next();
     });
 });
 
 router
   // GET /admin/drivers
-  .get('/admin/drivers', function(request, response, next) {
+  .get('/admin/drivers', function(request, response) {
     new DriverQuery()
       .findAll()
       .then(function(drivers){
@@ -28,18 +28,23 @@ router
   })
 
   // GET /admin/drivers/:userId
-  .get('/admin/drivers/:userId', function(request, response, next) {
-    response.render('admin/drivers/show', { driver: request.user });
+  .get('/admin/drivers/:userId', function(request, response) {
+    response.render('admin/drivers/show', { driver: request.driver });
+  })
+
+  // GET /admin/drivers/:userId/edit
+  .get('/admin/drivers/:userId/edit', function(request, response) {
+    response.render('admin/drivers/edit', { driver: request.driver });
   })
 
   // GET /admin/drivers/new
-  .get('/admin/drivers', function(request, response, next) {
+  .get('/admin/drivers', function(request, response) {
       var driverForm = new DriverForm(null);
       response.render('/admin/users/new')
   })
 
   // POST /admin/drivers
-  .post('/admin/drivers', function(request, response, next) {
+  .post('/admin/drivers', function(request, response) {
     var formParams;
     var form = new DriverForm(request, response);
 
