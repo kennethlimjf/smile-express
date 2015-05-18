b244fe5a60a6b30e8b3e21bd031d676dfcff1cf0
Kenneth Lim <kennethlimjf@gmail.com>
2015-05-18 20:44:21 +0800

    Better seed for populating dev database

--- config/seed.js
+++ config/seed.js
@@ -1,44 +1,93 @@
-var db = require("../models");
+const
+  db    = require('../models'),
+  async = require('async'),
+  _     = require('underscore'),
+  Q     = require('q'),
+  faker = require('faker');
 
-db.sequelize
-  .sync({force: true})
-  .then(function() {
-    createUser();
-    createAdminUser();
-  });
+var Seed = function(){};
 
-function createUser() {
-  db.User
-    .create({
-      email: 'john@smith.com',
-      firstName: 'John',
-      lastName: 'Smith',
-      nickname: 'Johnny',
-      shortDesc: 'This is a short description',
-      description: 'This is a description',
-      phone: '+12345678'
-    })
-    .then(function(user) {
-      user1 = user;
-      db.Profile
-        .create({
-          UserId: user.id,
-          profileType: 'DriverProfile',
-          data: {
-            languagesSpoken: ['English', 'Khmer'],
-            languagesWritten: ['Khmer'],
-            drivingExperience: 10,
-            cities: ['Siem Reap', 'Phnom Penh'],
-            vehicles: ['Tuk Tuk', 'Car', 'Mini Bus']
-          }
-        });
-    });
-}
+Seed.prototype = _.extend(Seed.prototype, {
+  run: function() {
+    var _this = this;
 
-function createAdminUser() {
-  db.AdminUser
-    .create({
-      email: 'admin',
-      password: 'admin'
-    })
-}
+    _this.createUsers      = _.range(20).map(n => _this.createUser);
+    _this.createAdminUsers = _.range(1).map(n => _this.createAdminUser);
+    _this.tasks            = _this.createUsers.concat(_this.createAdminUsers);
+
+    this
+      .forceSync()
+      .then(function(){
+        async.parallel(_this.tasks, _this.done);
+      });
+  },
+
+  forceSync: function() {
+    this.forceSyncDeferred = Q.defer();
+    var _this = this;
+
+    db.sequelize
+      .sync({ force: true })
+      .then(function() {
+        _this.synced(null, true);
+      });
+
+    return this.forceSyncDeferred.promise;
+  },
+
+  synced: function(err, result) {
+    if(err) {
+      this.forceSyncDeferred.reject(err);
+    } else {
+      this.forceSyncDeferred.resolve(result);
+    }
+  },
+
+  createUser: function(callback) {
+    db.User
+      .create({
+        email: faker.internet.email(),
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        nickname: faker.name.findName(),
+        shortDesc: faker.lorem.sentence(),
+        description: faker.lorem.paragraph(),
+        phone: faker.phone.phoneNumber()
+      })
+      .then(function(user) {
+        db.Profile
+          .create({
+            UserId: user.id,
+            profileType: 'DriverProfile',
+            data: {
+              languagesSpoken: ['English', 'Khmer'],
+              languagesWritten: ['Khmer'],
+              drivingExperience: 10,
+              cities: ['Siem Reap', 'Phnom Penh'],
+              vehicles: ['Tuk Tuk', 'Car', 'Mini Bus']
+            }
+          })
+          .then(function(){
+            callback(null, true);
+          });
+      });
+  },
+
+  createAdminUser: function(callback) {
+    db.AdminUser
+      .create({
+        email: 'admin',
+        password: 'admin'
+      })
+      .then(function() {
+        callback(null, true);
+      });
+  },
+
+  done: function(err) {
+    if(err) { throw err; }
+    else { console.log('Seed Complete!'); }
+  }
+});
+
+new Seed().run();
